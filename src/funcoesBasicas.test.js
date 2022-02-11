0c9ba6dc016a05f18b9810fd321ec1bd7d1bf7a2
MatheusD <55540506+MatheusDama@users.noreply.github.com>
2022-02-11 15:54:42 -0300

    aula02

--- src/funcoesBasicas.test.js
+++ src/funcoesBasicas.test.js
@@ -10,23 +10,27 @@ beforeAll(async () => {
   browser = await chromium.launch({ headless: false }); // por default vem true
   // contexto é o nosso navegador em si
   context = await browser.newContext();
+
+  page = await context.newPage();
+  await page.goto("https://letcode.in/test");
 });
 // executa antes de cada teste
 beforeEach(async () => {
   // finalmente a nossa page onde
-  page = await context.newPage();
-  await page.goto("https://letcode.in/test");
+  // page = await context.newPage();
+  // await page.goto("https://letcode.in/test");
 });
 
 // executa depois de cada teste
 afterEach(async () => {
   // fecha a page
-  await page.close();
+  // await page.close();
+
+  // fecha o browser
+  await browser.close();
 });
 // executa depois de todos os testes
 afterAll(async () => {
-  // fecha o contexto
-  await context.close();
   // fecha o browser
   await browser.close();
 });
@@ -83,5 +87,72 @@ describe("Funções básicas do playwright", () => {
     });
 
     await element.click();
+  });
+
+  test("Waits", async () => {
+    await page.click(selectors.btnClickTestFill);
+    /**
+     * locator retorna um locator do playwright com ele podemos rodar métodos diretamente com ele
+     * exemplo:
+     * const button = await page.locator('#button')
+     * await button.click()
+     * waitFor é uma espera dinâmica que espera um estado de opções
+     * exemplo:
+     * const button = await page.locator('#button').waitFor()
+     */
+    await page.locator("input#fullName").waitFor("visible");
+
+    console.time();
+    // espera dinâmica por um seletor
+    await page.waitForSelector("input#fullName", 3000);
+
+    // espera fixa
+    await page.waitForTimeout(5000);
+    console.timeEnd();
+  });
+
+  test("Selects", async () => {
+    await page.click("a[href='/dropdowns']");
+
+    // select comum
+    // método $ ele encontra um elemento que corresponde ao seletor
+    const select = await page.$("#fruits");
+    const option = await page.$("option[value='2']");
+    // setamos o select e usamos o metodo abaixo e passamos como parametro o option
+    await select.selectOption(option);
+
+    // multiple selects
+    const selectMultiple = await page.$("#superheros");
+    await selectMultiple.selectOption([
+      { label: "Ant-Man" },
+      { value: "bt" },
+      { index: 8 },
+    ]);
+  });
+
+  test("locators", async () => {
+    console.time();
+    const edit = await page.locator("a[href='/edit']");
+    console.log(`edit value == ${await edit.textContent()}`);
+
+    const buttons = await page.locator("a[href='/buttons']");
+    console.log(`button value == ${await buttons.textContent()}`);
+
+    // captura multiplos elementos
+    const cards = await page.$$("div[class='column is-3-desktop is-6-tablet']");
+    console.log(`cards com $$ == ${cards.length}`);
+    console.timeEnd();
+  });
+
+  test("checks", async () => {
+    await page.click("a[href='/radio']");
+
+    // insere um check no radio
+    await page.check("input#yes");
+
+    await page.waitForTimeout(2000);
+
+    // retira um check no radio
+    await page.uncheck("xpath=(//div[@class='field'])[6]/label[2]/input");
   });
 });
